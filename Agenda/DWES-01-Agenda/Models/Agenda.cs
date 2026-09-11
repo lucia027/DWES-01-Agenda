4db089eed3b9aea07dbf92eae95ceb282d3f27b5
Lucía Fuertes Cruz <lucia.fuertes.06@gmail.com>
2026-09-11 17:55:20 +0200

    Cambio de nombre de solucion y proyectos.

---
+++ Agenda/DWES-01-Agenda/Models/Agenda.cs
@@ -0,0 +1,9 @@
+﻿namespace DWES;
+
+public record Agenda {
+    public int Id {get; init;}
+    public string Alias { get; init; } = string.Empty;
+    public string Nombre {get; init;} =  string.Empty;
+    public string Telefono {get; init;} =  string.Empty;
+    public string Email {get; init;} =  string.Empty;
+}
